902fb3c18ddbaa332ceefa547d54a3a7159a78f5
maindeyo <idkharrw@gmail.com>
2024-11-24 17:48:22 -0300

    Criação da Navbar e estruturação

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Login from './Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Login from './components/Login';
+import Navigation from './components/Navigation';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <Navigation />
     <Login />
+    
   </React.StrictMode>
 );
 
