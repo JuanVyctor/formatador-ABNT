bad505aa22f829f8ac2e5a9cf576eebaf987d324
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2024-12-01 16:42:43 -0300

    summernote funcionando

--- frontend/src/components/Summernote.js
+++ frontend/src/components/Summernote.js
@@ -1,43 +1,84 @@
-import React, { useEffect, useRef } from 'react';
-import $ from 'jquery';
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import 'summernote/dist/summernote-lite.css';
-import 'summernote/dist/summernote-lite.js';
-import { Container, Button, Card } from 'react-bootstrap';  
+import React, { useEffect, useRef, useState } from "react";
+import $, { Callbacks } from "jquery";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "summernote/dist/summernote-lite.css";
+import "summernote/dist/summernote-lite.js";
+import { Container, Button, Card } from "react-bootstrap";
+import { useForm } from 'react-hook-form';
+import api from "../services/api";
 
 const SummernoteComponent = () => {
+
+  const { register, handleSubmit, setValue } = useForm();
   const editorRef = useRef(null);
-
+  
   useEffect(() => {
-    $(editorRef.current).summernote({
-      height: 300,
-      placeholder: 'Digite seu texto aqui...',
-      toolbar: [
-        ['style', ['style']],
-        ['font', ['bold', 'italic', 'underline', 'clear']],
-        ['fontname', ['fontname']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        ['table', ['table']],
-        ['insert', ['link', 'picture', 'video']],
-        ['view', ['fullscreen', 'codeview', 'help']]
-      ],
+      $(editorRef.current).summernote({
+          height: 300,
+          placeholder: "Digite seu texto aqui...",
+          toolbar: [
+        ["style", ["style"]],
+        ["font", ["bold", "italic", "underline", "clear"]],
+        ["fontname", ["fontname"]],
+        ["color", ["color"]],
+        ["para", ["ul", "ol", "paragraph"]],
+        ["table", ["table"]],
+        ["insert", ["link", "picture", "video"]],
+        ["view", ["fullscreen", "codeview", "help"]],
+    ],
+    callbacks: {
+        onChange: function (content, $edidable) {
+            setValue('texto', content);
+        },
+      },
     });
-
     return () => {
-      $(editorRef.current).summernote('destroy');
+      $(editorRef.current).summernote("destroy");
     };
   }, []);
+    
+    const addDoc = data => 
+      api.post("/documentos", data)
+    .then(() => {
+      console.log('deu certo');
+    }).catch(() => {
+      console.log('deu errado');
+    });
 
   return (
-    <Container className="mt-5 text-center">
-      <Card className="p-4" style={{ backgroundColor: '#A583F5', color: 'white', borderRadius: '20px' }}>
-        <div ref={editorRef}></div>
-      </Card>
-      <Button variant="light" className="mt-4" style={{ border: '2px solid black' }}>
-        Formatar
-      </Button>
-    </Container>
+    <form onSubmit={handleSubmit(addDoc)}>
+      <Container className="mt-5 text-center">
+        <Card
+          className="p-4"
+          style={{
+            backgroundColor: "#A583F5",
+            color: "white",
+            borderRadius: "20px",
+          }}
+        >
+          <div ref={editorRef} />
+        </Card>
+        <input
+          type="hidden"
+          name="texto"
+          {...register("texto")}
+        ></input>
+        <input
+          type="hidden"
+          value="1"
+          name="usu_id"
+          {...register("usu_id")}
+        ></input>
+        <Button
+          variant="light"
+          className="mt-4"
+          type="submit"
+          style={{ border: "2px solid black" }}
+        >
+          Formatar
+        </Button>
+      </Container>
+    </form>
   );
 };
 
