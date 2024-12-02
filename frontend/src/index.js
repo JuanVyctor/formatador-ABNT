f1a10a1d3ff26f87cba92283dc6cd5c42c74e2a6
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2024-12-01 21:54:12 -0300

    arruma index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,11 +16,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navigation />
-    {/* <Login />
+    <Login />
     <SignUp />
     <Account />
-    <SummernoteComponent /> */}
-    <SummernoteComponentPut />
+    <SummernoteComponent />
+    {/* <SummernoteComponentPut /> */}
   </React.StrictMode>
 );
 
