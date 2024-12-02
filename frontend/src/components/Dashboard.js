7d2c991bd601b45582946ee575066e9e235ec612
itad0risa <mariaisauramorais8@gmail.com>
2024-12-02 15:46:57 -0300

    .

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -5,19 +5,43 @@ import newDocument from '../newDocument.png';
 import imagem from '../imagem.jpg';
 import "../css/Dashboard.css";
 
+ function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+
+
+
+
+
+
+
+
 function Grid() {
   return (
-  <>
-      
+  <>  
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={newDocument} />
-    </Card>
-            <Card style={{ width: '10rem' }}>
-              <Card.Body>
-                <Card.Img src={imagem} />
-              </Card.Body>
-            </Card>
-            </>
+        <Card.Img variant="top" src={newDocument} />
+      </Card>
+      <Card style={{ width: '10rem' }}>
+        <Card.Body>
+          <Card.Img src={imagem} />
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
