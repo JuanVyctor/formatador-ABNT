f87efe6acd380729ac3d64fc0ce212c13fa52a73
maindeyo <idkharrw@gmail.com>
2024-12-03 21:39:59 -0300

    Implementação login + logout

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,6 +6,12 @@ use App\Models\Documento;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Services\ResponseService; 
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
@@ -34,23 +40,19 @@ class UserController extends Controller
     }
 
     public function login(Request $request) {
-        $dados = $request->validate([
-            'email' => ['required', 'email'],
-            'senha' => ['required'],
-        ]);
-
-        $u = User::where('email', $dados['email'])->where('senha', $dados['senha']);
-        
-        // if (Auth::attempt($dados)) {
-            $request->session()->regenerate();
- 
-            // return redirect()->intended('dashboard');
-        // }
-
-        return response(
-            ['location' => ('usuarios/'. $u->id)], 200
-        );
+        $credentials = $request->only('email', 'senha');
+        try {
+            $user = User::where('email', $credentials['email'])->first();
+            if (!$user || !Hash::check($credentials['senha'], $user->senha)) {
+                return response()->json(['message' => 'Credenciais incorretas, verifique-as e tente novamente.'], 401);
+            }
+        $token = JWTAuth::fromUser($user);
+        } catch (\Throwable|\Exception $e) {
+            return ResponseService::exception('users.login', null, $e);
+        }
+        return response()->json(compact('token'));
     }
+
 
     public function show(int $id) {
         $usuario = User::find($id);
@@ -92,4 +94,18 @@ class UserController extends Controller
             return response(status: 404);
         $usuario->delete();
     }
+
+    public function logout(Request $request) {
+        try {
+            $token = $request->bearerToken(); //recebe o token do cabeçalho
+            if (!JWTAuth::invalidate($token)) { //tenta invalidar o token
+                throw new \Exception('Erro. Tente novamente.', -404);
+            }
+            return response(['status' => true, 'msg' => 'Deslogado com sucesso'], 200);
+        } catch (\Throwable|\Exception $e) {
+            return ResponseService::exception('users.logout', null, $e);
+        }
+    }
+    
+
 }
